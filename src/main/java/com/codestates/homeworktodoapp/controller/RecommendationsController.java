a1cafbc8deb16371e7698a8912034f2b295a0ef2
kim changha <ckdgk0366@gmail.com>
2023-02-14 16:08:08 +0900

    first

---
+++ src/main/java/com/codestates/homeworktodoapp/controller/RecommendationsController.java
@@ -0,0 +1,13 @@
+package com.codestates.homeworktodoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationsController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application";
+    }
+}
---
+++ src/test/java/com/codestates/homeworktodoapp/HomeworkTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.homeworktodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
